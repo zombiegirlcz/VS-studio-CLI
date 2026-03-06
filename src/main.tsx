adfcebd3d76048995e4c1061e96ed86d4ff86f13
zombiegirlcz <zombiegirlcz@gmail.com>
2026-03-06 06:39:17 +0100

    Revert debug changes - restore original app

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SimpleApp from './app/App.simple'
+import App from './app/App'
 import './index.css'
 
-try {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <SimpleApp />
-    </React.StrictMode>,
-  )
-} catch (e) {
-  console.error('React render error:', e)
-  const root = document.getElementById('root')
-  if (root) {
-    root.innerHTML = `<div style="color: red; padding: 20px;">ERROR: ${String(e)}</div>`
-  }
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
